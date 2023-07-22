c0daa9a01511b8f30d841ea01c21430a699d1639
oscarchavez99 <oscar.chavez4068@alumnos.udg.mx>
2023-07-21 18:18:44 -0600

    final version

--- CS-Students-Crud/ClMySqlCon.cs
+++ CS-Students-Crud/ClMySqlCon.cs
@@ -1,9 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CS_Students_Crud
--- CS-Students-Crud/Form1.cs
+++ CS-Students-Crud/Form1.cs
@@ -1,5 +1,4 @@
-﻿using MySqlX.XDevAPI.Relational;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
